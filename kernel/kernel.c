dcc11a631f9832eeb82ae3e66c3a2dbdcd39346c
AcePeaX <benmansour.mohammed2002@gmail.com>
2024-11-06 12:40:54 +0100

    Added video scrolling down - and copy memory and int_to_ascii in utils

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,19 @@
 #include "../drivers/screen.h"
-
+#include "util.h"
 
 void main() {
     clear_screen();
     kprint("Welcome to CasseOS!");
+    /* Fill up the screen */
+    int i = 0;
+    for (i = 0; i < 24; i++) {
+        char str[255];
+        int_to_ascii(i, str);
+        kprint_at(str, 0, i+1);
+        for (int l = 0; l < 32000; l++) {
+            for (int j = 0; j < 3200; j++) {
+            }
+        }
+    }
+    kprint("\nNow it switches");
 }
