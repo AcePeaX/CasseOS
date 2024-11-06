5b37d0c107572c411af61e41291a220592e821f2
AcePeaX <benmansour.mohammed2002@gmail.com>
2024-11-06 14:18:32 +0100

    Added timer and keyboard scan code

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,30 +2,16 @@
 #include "util.h"
 #include "../cpu/isr.h"
 #include "../cpu/idt.h"
+#include "../cpu/timer.h"
+#include "../drivers/keyboard.h"
 
 void main() {
     isr_install();
     clear_screen();
-    __asm__ __volatile__("int $2");
-    __asm__ __volatile__("int $3");
-    kprint("Welcome to CasseOS!\n");
-    // Wait a bit
-    for (int l = 0; l < 32000; l++) {
-        for (int j = 0; j < 8200; j++) {
-        }
-    }
-    /* Fill up the screen */
-    int i = 0;
-    for (i = 0; i < 201; i++) {
-        char str[255];
-        int_to_ascii(i, str);
-        kprint(str);
-        kprint("\n");
-        int wait_iter = 150+7200/(i+1);
-        for (int l = 0; l < 32000; l++) {
-            for (int j = 0; j < wait_iter; j++) {
-            }
-        }
-    }
-    kprint("\nSeems like working");
+
+    asm volatile("sti");
+    init_timer(50);
+    /* Comment out the timer IRQ handler to read
+     * the keyboard IRQs easier */
+    init_keyboard();
 }
--- kernel/util.c
+++ kernel/util.c
@@ -1,8 +1,15 @@
+#include "util.h"
+
 void memory_copy(char *source, char *dest, int nbytes) {
     int i;
     for (i = 0; i < nbytes; i++) {
         *(dest + i) = *(source + i);
     }
+}
+
+void memory_set(u8 *dest, u8 val, u32 len) {
+    u8 *temp = (u8 *)dest;
+    for ( ; len != 0; len--) *temp++ = val;
 }
 
 /**
@@ -19,12 +26,22 @@ void int_to_ascii(int n, char str[]) {
     if (sign < 0) str[i++] = '-';
     str[i] = '\0';
 
-    /* Reversing */
-    int end = i/2;
-    char t;
-    for(int j=0;j<end;j++){
-        t = str[j];
-        str[j] = str[i-j-1];
-        str[i-j-1] = t;
+    reverse(str);
+}
+
+/* K&R */
+void reverse(char s[]) {
+    int c, i, j;
+    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
     }
+}
+
+/* K&R */
+int strlen(char s[]) {
+    int i = 0;
+    while (s[i] != '\0') ++i;
+    return i;
 }
